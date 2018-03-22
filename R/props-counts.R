a893d6fb620f384f3a1d4acf322ceaccd67e0270
Randall Pruim <rpruim@gmail.com>
2018-03-22 12:03:06 -0400

    add factor method for counts.  Convert to factor for default method.

--- R/props-counts.R
+++ R/props-counts.R
@@ -38,8 +38,40 @@ counts <- function(x, ...) {
 #' @rdname props
 #' @export
 
+counts.factor <-
+  function(x, ..., format = c("count", "proportion", "percent")) {
+    format = match.arg(format)
+    uval <- levels(x)
+
+    res <- sapply(uval, function(v) base::sum(x == v, na.rm = TRUE))
+    names (res) <-
+      paste0(
+        switch(format, count = "n_", proportion = "prop_", percent = "perc_"),
+        as.character(uval)
+      )
+
+    n_missing <- base::sum(is.na(x), na.rm = TRUE)
+    if (n_missing > 0L) {
+      names(n_missing) <-
+        switch(format, count = "n_NA", proportion = "prop_NA", percent = "perc_NA")
+      res <- c(res, n_missing)
+    }
+    # do arithmetic to convert to proportions or percents, and return result
+    switch(
+      format,
+      count = res,
+      proportion =   res / length(x),
+      percent =  100 * res / length(x)
+    )
+  }
+
+
+#' @rdname props
+#' @export
+
 counts.default <-
   function(x, ..., format = c("count", "proportion", "percent")) {
+    return(counts(factor(x), ..., format = format))
     format = match.arg(format)
     uval <- sort(unique(x))
 

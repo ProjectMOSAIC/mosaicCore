7ea38e3767d9e200dcdc7c669c49ed1bdd49698e
Randall Pruim <rpruim@gmail.com>
2018-05-21 16:40:59 -0400

    add n_not_missing and n_total

--- R/nmissing.R
+++ R/nmissing.R
@@ -1,10 +1,11 @@
 
-#' counting missing elements
+#' Counting missing/non-missing elements
 #'
-#' counting missing elements
+#' Counting missing/non-missing elements
 #'
 #' @param type one of `"any"` (default) or `"all"`.
 #' @param ... vectors of equal length to be checked in parallel for missing values.
+#' @return a vector of counts of missing or non-missing values.
 #'
 #' @export
 #' @examples
@@ -18,10 +19,28 @@
 #'       missing.WtOrHt = n_missing(Weight, Height, type = "any")
 #'       )
 #'     }
+
 n_missing <- function( ..., type = c("any", "all")) {
   type <- match.arg(type)
   M <- cbind(...)
   base::sum(
     apply(M, 1, function(x) do.call(type, list(is.na(x)))),
     na.rm = TRUE)
+}
+
+#' @rdname n_missing
+#' @export
+n_not_missing <- function( ..., type = c("any", "all")) {
+  type <- match.arg(type)
+  M <- cbind(...)
+  base::sum(
+    1 - apply(M, 1, function(x) do.call(type, list(is.na(x)))),
+    na.rm = TRUE)
+}
+
+#' @rdname n_missing
+#' @export
+n_total <- function( ..., type = c("any", "all")) {
+  M <- cbind(...)
+  nrow(M)
 }
